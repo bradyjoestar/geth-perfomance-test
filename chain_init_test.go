4ae2c953f6381f3e11087e45134776ab3d3134e8
bradyjoestar <bradyjoestar@gmail.com>
2023-03-07 15:16:19 +0800

    wb: fix privatekey prefix

--- chain_init_test.go
+++ chain_init_test.go
@@ -16,7 +16,7 @@ func TestInitBitAccount(t *testing.T) {
 	l2client, err := utils.GetRpcClient(l2url, l2ChainID)
 	require.NoError(t, err, "l2client error")
 	userAddress := "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
-	UserPrivateKey := "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+	UserPrivateKey := "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
 
 	//distribute bit
 	utils.DistributeBit(l2client, userAddress, UserPrivateKey, t)
